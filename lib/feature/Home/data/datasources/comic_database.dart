59e8d9b0fd71953a3053b38536e958250401946b
Evinsson Medrano <evinssonmedrano@gmail.com>
2025-11-13 18:39:09 -0600

    feat: add update comic method

--- lib/feature/Home/data/datasources/comic_database.dart
+++ lib/feature/Home/data/datasources/comic_database.dart
@@ -29,37 +29,35 @@ class ComicDatabase {
 
   Future _createDatabase(Database db, int version) async {
     await db.execute('''
-      CREATE TABLE ${ComicFields.tableName} (
-        ${ComicFields.id} ${ComicFields.idType},
-        ${ComicFields.filePath} ${ComicFields.textType},
-        ${ComicFields.title} ${ComicFields.textType},
-        ${ComicFields.picture} TEXT,
-        ${ComicFields.currentPage} ${ComicFields.intType},
-        ${ComicFields.totalPages} ${ComicFields.intType},
-        ${ComicFields.lastOpened} ${ComicFields.intType},
-        ${ComicFields.currentReading} ${ComicFields.intType}
-        ${ComicFields.bookMarks} ${ComicFields.textType}
-        ${ComicFields.isFavorite} ${ComicFields.intType}      
-        ${ComicFields.imagesPath} ${ComicFields.textType} 
-        ${ComicFields.rating} ${ComicFields.intType}
-        ${ComicFields.isReading} ${ComicFields.textType}
-        ${ComicFields.isCompleted} ${ComicFields.textType}
-      )
-    ''');
+        CREATE TABLE ${ComicFields.tableName} (
+          ${ComicFields.id} ${ComicFields.idType},
+          ${ComicFields.filePath} ${ComicFields.textType},
+          ${ComicFields.title} ${ComicFields.textType},
+          ${ComicFields.picture} TEXT,
+          ${ComicFields.currentPage} ${ComicFields.intType},
+          ${ComicFields.totalPages} ${ComicFields.intType},
+          ${ComicFields.lastOpened} ${ComicFields.intType},
+          ${ComicFields.currentReading} ${ComicFields.intType}
+          ${ComicFields.bookMarks} ${ComicFields.textType}
+          ${ComicFields.isFavorite} ${ComicFields.intType}      
+          ${ComicFields.imagesPath} ${ComicFields.textType} 
+          ${ComicFields.rating} ${ComicFields.intType}
+          ${ComicFields.isReading} ${ComicFields.textType}
+          ${ComicFields.isCompleted} ${ComicFields.textType}
+        )
+      ''');
   }
 
   Future<int> addComic(ComicModel comic) async {
     final db = await database;
-
     int id = await db.insert('comics', comic.toMap());
     final result = await db.query('comics');
-
     return id;
   }
 
   Future<List<ComicModel>> fetchAllComics() async {
     final db = await database;
-    final maps = await db.query('comics');
+    final maps = await db.query(ComicFields.tableName);
     return List.generate(maps.length, (i) => ComicModel.fromMap(maps[i]));
   }
 
@@ -85,6 +83,29 @@ class ComicDatabase {
           "Comic ID: ${result.first['id']} - Current Page actualizado: ${result.first['currentPage']}");
     } else {
       print("No se encontró el cómic con ID: $id");
+    }
+  }
+
+  Future<void> updateComic({
+    required int id,
+    String? imagesPath,
+    String? filePath,
+    String? title,
+    int? totalPages,
+  }) async {
+    final db = await database;
+    final Map<String, Object?> values = {};
+    if (imagesPath != null) values[ComicFields.imagesPath] = imagesPath;
+    if (filePath != null) values[ComicFields.filePath] = filePath;
+    if (title != null) values[ComicFields.title] = title;
+    if (totalPages != null) values[ComicFields.totalPages] = totalPages;
+    if (values.isNotEmpty) {
+      await db.update(
+        ComicFields.tableName,
+        values,
+        where: '${ComicFields.id} = ?',
+        whereArgs: [id],
+      );
     }
   }
 }
