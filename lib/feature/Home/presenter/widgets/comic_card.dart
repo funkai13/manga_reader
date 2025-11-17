3968fb24f62bd3da339300e467f9add9dd8206cf
Evinsson Medrano <evinssonmedrano@gmail.com>
2025-11-16 22:32:07 -0600

    feat: change ui implements screen_utils

--- lib/feature/Home/presenter/widgets/comic_card.dart
+++ lib/feature/Home/presenter/widgets/comic_card.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:manga_reader/core/theme/colors.dart';
 import 'package:manga_reader/feature/Home/domain/entity/comic.dart';
 
 class ComicCard extends StatelessWidget {
@@ -10,19 +12,103 @@ class ComicCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Expanded(
-          child: comic.picture.isNotEmpty
-              ? Image.file(
-                  File(comic.picture),
-                  fit: BoxFit.cover,
-                )
-              : Container(
-                  color: Colors.grey[300],
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.r),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.15),
+                  blurRadius: 12,
+                  offset: const Offset(0, 4),
                 ),
-        )
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12.r),
+              child: comic.picture.isNotEmpty
+                  ? Image.file(
+                      File(comic.picture),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Container(
+                          color: isDark
+                              ? AppColorsDark.cardColor
+                              : AppColorsLight.cardColor,
+                          child: Icon(
+                            Icons.image_not_supported,
+                            color: isDark
+                                ? AppColorsDark.textColor.withOpacity(0.3)
+                                : AppColorsLight.textColor.withOpacity(0.3),
+                            size: 40.sp,
+                          ),
+                        );
+                      },
+                    )
+                  : Container(
+                      color: isDark
+                          ? AppColorsDark.cardColor
+                          : AppColorsLight.cardColor,
+                      child: Icon(
+                        Icons.book,
+                        color: isDark
+                            ? AppColorsDark.textColor.withOpacity(0.3)
+                            : AppColorsLight.textColor.withOpacity(0.3),
+                        size: 40.sp,
+                      ),
+                    ),
+            ),
+          ),
+        ),
+        SizedBox(height: 12.h),
+        Text(
+          comic.title,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontSize: 14.sp,
+            fontWeight: FontWeight.w600,
+            color: isDark
+                ? AppColorsDark.textColor
+                : AppColorsLight.textColor,
+            height: 1.3,
+          ),
+        ),
+        if (comic.isReading)
+          Padding(
+            padding: EdgeInsets.only(top: 6.h),
+            child: Row(
+              children: [
+                Container(
+                  width: 6.w,
+                  height: 6.w,
+                  decoration: BoxDecoration(
+                    color: isDark
+                        ? AppColorsDark.accentColor
+                        : AppColorsLight.accentColor,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                SizedBox(width: 6.w),
+                Text(
+                  'Leyendo',
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    color: isDark
+                        ? AppColorsDark.accentColor
+                        : AppColorsLight.accentColor,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
       ],
     );
   }
--- lib/feature/Home/presenter/widgets/comics_carousel.dart
+++ lib/feature/Home/presenter/widgets/comics_carousel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:manga_reader/core/theme/colors.dart';
 import 'package:manga_reader/feature/Home/domain/entity/comic.dart';
 import 'package:manga_reader/feature/Home/presenter/screen/comic_viewer_screen.dart';
 
@@ -12,38 +14,56 @@ class ComicsCarousel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (comics.isEmpty) return const SizedBox.shrink();
+
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          textAlign: TextAlign.start,
-          title,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 22.sp,
+              fontWeight: FontWeight.bold,
+              color: isDark
+                  ? AppColorsDark.textColor
+                  : AppColorsLight.textColor,
+              letterSpacing: -0.5,
+            ),
           ),
         ),
+        SizedBox(height: 16.h),
         SizedBox(
-          height: 250,
-          width: 500,
-          child: CarouselView.weighted(
-              onTap: (
-                int index,
-              ) {
-                Navigator.push(
+          height: 280.h,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            physics: const BouncingScrollPhysics(),
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            itemCount: comics.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) =>
-                            ComicViewerScreen(comic: comics.elementAt(index))));
-              },
-              scrollDirection: Axis.horizontal,
-              flexWeights: [1, 1],
-              shrinkExtent: 200,
-              itemSnapping: true,
-              shape: Border(),
-              children: List<Widget>.generate(comics.length,
-                  (int index) => ComicCard(comic: comics[index]))),
+                      builder: (context) =>
+                          ComicViewerScreen(comic: comics[index]),
+                    ),
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.only(right: 16.w),
+                  width: 160.w,
+                  child: ComicCard(comic: comics[index]),
+                ),
+              );
+            },
+          ),
         ),
+        SizedBox(height: 32.h),
       ],
     );
   }
